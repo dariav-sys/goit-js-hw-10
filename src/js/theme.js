bcfaad9ed69991de20df23a3814009d0cd5b9035
Daria Viter <viter.daria@gmail.com>
2021-01-28 12:01:49 +0000

    finish

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,24 +5,50 @@ const Theme = {
 };
 
 const checkbox = document.querySelector('#theme-switch-toggle');
-checkbox.addEventListener('change', switchTheme);
-
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === Theme.DARK) {
-    document.body.classList.toggle(Theme.DARK);
-    checkbox.checked = true;
-} else {
-    document.body.classList.toggle(Theme.LIGHT);
-    checkbox.checked = false;
-}
-
-function switchTheme(event) {
-    let checkboxState = event.currentTarget.checked;
-    if (checkboxState) {        
+checkbox.addEventListener('click', switchTheme);
+function switchTheme() { 
+    if (document.body.classList.contains(Theme.LIGHT)) {
         document.body.classList.replace(Theme.LIGHT, Theme.DARK);
         localStorage.setItem('theme', Theme.DARK);
     } else {
-        document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+        document.body.classList.replace(Theme.DARK, Theme.LIGHT); 
         localStorage.setItem('theme', Theme.LIGHT);
-    }
+    }       
+};
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === Theme.DARK) {
+    document.body.classList.add(Theme.DARK);
+    checkbox.checked = true;
+} else {
+    document.body.classList.add(Theme.LIGHT); 
 }
+
+
+
+
+
+
+
+// const checkbox = document.querySelector('#theme-switch-toggle');
+// checkbox.addEventListener('change', switchTheme);
+
+// const savedTheme = localStorage.getItem('theme');
+// if (savedTheme === Theme.DARK) {
+//     document.body.classList.toggle(Theme.DARK);
+//     checkbox.checked = true;
+// } else {
+//     document.body.classList.toggle(Theme.LIGHT);
+//     checkbox.checked = false;
+// }
+
+// function switchTheme(event) {
+//     let checkboxState = event.currentTarget.checked;
+//     if (checkboxState) {        
+//         document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+//         localStorage.setItem('theme', Theme.DARK);
+//     } else {
+//         document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+//         localStorage.setItem('theme', Theme.LIGHT);
+//     }
+// }
